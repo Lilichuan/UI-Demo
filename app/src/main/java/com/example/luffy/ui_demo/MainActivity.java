5a467cf5e83b1b4655c4225c24e3390720daeec2
Lilichuan <luffy74123@gmail.com>
2017-03-08 09:52:34 +0800

    Back up

--- app/src/main/java/com/example/luffy/ui_demo/MainActivity.java
+++ app/src/main/java/com/example/luffy/ui_demo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.luffy.ui_demo;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        changeToMainFragment();
+    }
+
+    private void changeToMainFragment(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, MainFragment.newInstance())
+                .commit();
     }
 }
--- app/src/main/java/com/example/luffy/ui_demo/ViewPagerWithDot/ColorFragment.java
+++ app/src/main/java/com/example/luffy/ui_demo/ViewPagerWithDot/ColorFragment.java
@@ -1,4 +1,4 @@
-package com.example.luffy.ui_demo;
+package com.example.luffy.ui_demo.ViewPagerWithDot;
 
 
 import android.os.Bundle;
@@ -7,19 +7,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.luffy.ui_demo.R;
+
+/**
+ *
+ * 只顯示色塊的Fragment
+ *
+ */
+public class ColorFragment extends Fragment {
+
+    private static final String ARG_COLOR = "param1";
+
+    private int m_color;
 
 
-public class MainFragment extends Fragment {
-
-
-    public MainFragment() {
+    public ColorFragment() {
         // Required empty public constructor
     }
 
 
-    public static MainFragment newInstance() {
-        MainFragment fragment = new MainFragment();
+    public static ColorFragment newInstance(int color_value) {
+        ColorFragment fragment = new ColorFragment();
         Bundle args = new Bundle();
+        args.putInt(ARG_COLOR, color_value);
         fragment.setArguments(args);
         return fragment;
     }
@@ -28,16 +38,16 @@ public class MainFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
+            m_color = getArguments().getInt(ARG_COLOR);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-
-        return view;
+        View v = inflater.inflate(R.layout.fragment_color, container, false);
+        v.setBackgroundColor(m_color);
+        return v;
     }
 
 }
